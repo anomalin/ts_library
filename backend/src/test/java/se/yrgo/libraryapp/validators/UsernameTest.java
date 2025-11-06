76817aa640d3b2921b0b72b97b8778889bcf3916
Malin Sundberg <anomalins@gmail.com>
2025-11-06 13:16:28 +0100

    additional test strings

--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,18 +1,15 @@
 package se.yrgo.libraryapp.validators;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UsernameTest {
     @ParameterizedTest
-    @ValueSource(strings={"bosse", "bossE--", "boss", "bo@0rm"})
-    void correctUsername(String nameString) {
+    @ValueSource(strings={"bosse", "bossE--", "boss", "bo@0rm", "bo1234"})
+    void validUsername(String nameString) {
         boolean result = Username.validate(nameString);
         assertThat(result).isTrue();
     }
@@ -20,7 +17,7 @@ public class UsernameTest {
     @ParameterizedTest
     @ValueSource(strings={"name with space", "   ", "??jfk", "bos", "båsse"})
     @EmptySource
-    void incorrectUsername(String nameString) {
+    void invalidUsername(String nameString) {
         boolean result = Username.validate(nameString);
         assertThat(result).isFalse();
     }
