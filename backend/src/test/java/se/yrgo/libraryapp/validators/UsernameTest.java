706046b724b73b6c20378250de891293b5e3c732
Malin Sundberg <anomalins@gmail.com>
2025-11-06 11:09:05 +0100

    Parameterized tests

--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,21 +1,31 @@
 package se.yrgo.libraryapp.validators;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UsernameTest {
-    @Test
-    void correctUsername() {
-        boolean result = Username.validate("bosse");
+    @ParameterizedTest
+    @ValueSource(strings={"bosse", "bossE--", "boss", "bo@0rm"})
+    void correctUsername(String nameString) {
+        boolean result = Username.validate(nameString);
         assertThat(result).isTrue();
     }
 
-    @Test
-    void incorrectUsername() {
-        boolean result = Username.validate("name with space");
+    @ParameterizedTest
+    @ValueSource(strings={"name with space", "   ", "??jfk", "bos", "båsse"})
+    @EmptySource
+    void incorrectUsername(String nameString) {
+        boolean result = Username.validate(nameString);
         assertThat(result).isFalse();
     }
+
+
 
 
 }
